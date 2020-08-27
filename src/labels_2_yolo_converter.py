5182da8c61b6a2553669f70ca80c372bd9b7005b
Vladimir Privalov <vladprivalov1990@gmail.com>
2020-08-27 15:21:26 +0200

    Fix labels_2_yolo_converter

--- src/labels_2_yolo_converter.py
+++ src/labels_2_yolo_converter.py
@@ -25,19 +25,26 @@ def convert_labels():
     ap = argparse.ArgumentParser()
     ap.add_argument("-c", "--class", required=True,
         help="Class name of objects")
-    ap.add_argument("-p", "--label_path", required=False,
+    ap.add_argument("-f", "--names_file", required=True,
+        help="File with class names")
+    ap.add_argument("-l", "--label_path", required=False,
         default="./dataset/labels",
         help="Path to label files")
-    ap.add_argument("-p", "--image_path", required=False,
-                    default="./dataset",
-                    help="Path to label files")
+    ap.add_argument("-i", "--image_path", required=False,
+        default="./dataset",
+        help="Path to label files")
     ap.add_argument("-o", "--out_path", required=False,
         default="./result",
         help="Path to output txt files")
 
     args = vars(ap.parse_args())
 
-    classes = ["bus", "tank", "tractor"]
+    classes = []
+    with open(args['names_file']) as f:
+        for line in f:
+            class_name = line.rstrip('\n')
+            print(class_name)
+            classes.append(class_name)
 
     """ Configure Paths"""
     class_name = args['class']
@@ -45,10 +52,9 @@ def convert_labels():
     # print(class_num)
     mypath = "%s/%s/" % (args['label_path'], class_name)
     outpath = "%s/%s/" % (args['out_path'], class_name)
-    # print(mypath)
-    # print(outpath)
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
 
-    wd = getcwd()
     list_file = open('%s_list.txt' % (class_name), 'w')
 
     """ Get input json file list """
@@ -59,7 +65,6 @@ def convert_labels():
 
     print(len(json_name_list))
 
-    i = 0
     for json_name in json_name_list:
         txt_name = json_name.rstrip(".json") + ".txt"
         file_path = mypath + json_name
@@ -69,7 +74,6 @@ def convert_labels():
         print("Output:" + txt_outpath)
         txt_outfile = open(txt_outpath, "a")
 
-        # img_path = str('%s/dataset/%s/%s.jpg' % (wd, class_name, os.path.splitext(json_name)[0]))
         img_path = str('%s/%s/%s.jpg' % (args['image_path'], class_name, os.path.splitext(json_name)[0]))
         if not os.path.exists(img_path):
             os.remove(file_path)
@@ -104,28 +108,22 @@ def convert_labels():
                 # print(xmin, xmax, ymin, ymax)
                 b = (xmin, xmax, ymin, ymax)
                 bb = convert((w, h), b)
-                if i == 0:
-                    print('bbox\n')
-                    print(b)
-                    print('bbox converted\n')
-                    print(bb)
                 # bb = (x, y, w, h)
                 # print(bb)
                 txt_outfile.write("%d %s\n" % (class_num, " ".join([str(a) for a in bb])))
                 processed_shapes.append(shape)
         txt_outfile.close()
         out_file_size = int(os.stat(txt_outpath).st_size)
+        
         if out_file_size == 0:
             print('Empty file')
             os.remove(txt_outpath)
         else:
             """ Save those images with bb into list"""
-            # list_file.write('%s/dataset/%s/%s.jpg\n' % (wd, class_name, os.path.splitext(txt_name)[0]))
             list_file.write('%s/%s/%s.jpg\n' % (args['image_path'], class_name, os.path.splitext(txt_name)[0]))
-
-        i += 1
 
     list_file.close()
 
+
 if __name__ == '__main__':
-    convert()
+    convert_labels()
